6d0d0b9cbc66a85f67575fecbf7b4210a04a1303
Kevyn <kevynjaremko@gmail.com>
2021-06-25 05:03:55 +0000

    First commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+
+from setuptools import setup
+
+setup(
+            name='mltest',
+                version='0.1',
+                    description='First Python package',
+                        url='#',
+                            author='auth',
+                                author_email='author@email.com',
+                                    license='MIT',
+                                        packages=['mltest'],
+                                            zip_safe=False
+                                            )
+
